3437e143bf931dccfb9077441d1b4a7f306a40fc
Paula A. de Vasconcelos Gueiros Bernardes <paulinhavgueiros@gmail.com>
2015-11-13 15:03:41 +0000

    Javascript testing

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>PHP Starter Application</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
+	<script src="script.js"></script>
 </head>
 <body>
 	<table>

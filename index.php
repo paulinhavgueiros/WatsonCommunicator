da843a3c0857448a3963022ee8c9f5d2fba704cf
Paula A. de Vasconcelos Gueiros Bernardes <paulinhavgueiros@gmail.com>
2015-11-15 01:27:38 +0000

    php e mysql

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link rel='stylesheet'  href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' type='text/css'>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     <script src="js/jquery.stellar.js"></script>
@@ -48,10 +49,86 @@
 	</div>
     
     <div class="section">
-        <h2>Heading First</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
-    </div>
+       
+
+
+	<h1>CF PHP MySQL Demo</h1>
+	<input type="button" onclick="window.open('init.php');" class="btn" value="Create table"></input>
+	<input type="button" onclick="window.open('info.php');" class="btn" value="View PHP info"></input>
+	</br>
+
+<?php	
+echo '<h2>VCAP_SERVICE Environment variable</h2>';
+echo "----------------------------------" . "</br>";
+$key = "VCAP_SERVICES";
+$value = getenv ( $key );
+echo $key . ":" . $value . "</br>";
+echo "----------------------------------" . "</br>";
+$vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
+$db = $vcap_services->{'mysql-5.5'}[0]->credentials;
+$mysql_database = $db->name;
+$mysql_port=$db->port;
+$mysql_server_name =$db->host . ':' . $db->port;
+$mysql_username = $db->username; 
+$mysql_password = $db->password; 
+
+$con = mysql_connect($mysql_server_name, $mysql_username, $mysql_password);
+if (!$con){
+    die ('connection failed' . mysql_error());
+}
+mysql_select_db($mysql_database,$con);   
+
+$strsq0 = "INSERT INTO ACCESS_HISTORY ( BROWSER, IP_ADDRESS) VALUES
+('" . $_SERVER['HTTP_USER_AGENT'] . "', '" . $_SERVER['REMOTE_ADDR'] . "');";
+$result0 = mysql_query ( $strsq0 );
+if ($result0) {
+	//echo "insert success!";
+} else {
+	echo "Cannot insert into the data table; check whether the table is created, or the database is active.";
+}
+$strsql = "select * from ACCESS_HISTORY ORDER BY ID DESC limit 100";
+
+$result = mysql_db_query ( $mysql_database, $strsql, $con );
+
+$row = mysql_fetch_row ( $result );
+
+echo '<h2>Access history</h2>';
+echo '<table class="table">';
+echo "\n<tr>\n";
+for($i = 0; $i < mysql_num_fields ( $result ); $i ++) {
+	echo '<th>' . mysql_field_name ( $result, $i );
+	echo "</th>\n";
+}
+echo "</tr>\n";
+
+mysql_data_seek ( $result, 0 );
+
+while ( $row = mysql_fetch_row ( $result ) ) {
+	echo "<tr>\n";
+	for($i = 0; $i < mysql_num_fields ( $result ); $i ++) {
+		echo '<td>';
+		echo "$row[$i]";
+		echo '</td>';
+		
+	}
+	echo "</tr>\n";
+}
+echo "</table>\n";
+
+mysql_free_result ( $result );
+mysql_close();
+?>
+ <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  
+  
+  
+
+
+
+
+
+	</div>
     
     <div id="bg2" class="backgroundImage" data-stellar-background-ratio="0.5"></div>
     

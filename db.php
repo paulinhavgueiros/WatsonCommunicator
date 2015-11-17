8b28c412bdf2378378cd34a8da70796ba1f2d047
Paula A. de Vasconcelos Gueiros Bernardes <paulinhavgueiros@gmail.com>
2015-11-17 18:23:36 +0000

    db correction

--- db.php
+++ db.php
@@ -1,33 +1,36 @@
 <?php
 
-if(!$_ENV["VCAP_SERVICES"]){ //local dev
-    $mysql_server_name = "127.0.0.1:3306";
+if (!$_ENV["VCAP_SERVICES"]) {
+	
+    $mysql_servername = "127.0.0.1:3306";
     $mysql_username = "root";
     $mysql_password = "";
     $mysql_database = "test";
+    
 } else { //running in Bluemix
+
     $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
-    if($vcap_services->{'mysql-5.5'}){ //if "mysql" db service is bound to this application
+    
+    if ($vcap_services->{'mysql-5.5'}) {
         $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
-    } 
-    else if($vcap_services->{'cleardb'}){ //if cleardb mysql db service is bound to this application
+    } else if ($vcap_services->{'cleardb'}) { // if cleardb mysql db
         $db = $vcap_services->{'cleardb'}[0]->credentials;
-    } 
-    else { 
-        echo "Error: No suitable MySQL database bound to the application. <br>";
+    } else { 
+        echo "MySQL database not found <br/>";
         die();
     }
+    
     $mysql_database = $db->name;
-    $mysql_port=$db->port;
-    $mysql_server_name =$db->hostname . ':' . $db->port;
+    $mysql_port = $db->port;
+    $mysql_servername = $db->hostname . ':' . $db->port;
     $mysql_username = $db->username; 
     $mysql_password = $db->password;
 }
-//echo "Debug: " . $mysql_server_name . " " .  $mysql_username . " " .  $mysql_password . "\n";
 
-$mysqli = new mysqli($mysql_server_name, $mysql_username, $mysql_password, $mysql_database);
+
+$mysqli = new mysqli($mysql_servername, $mysql_username, $mysql_password, $mysql_database);
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "Cannot connect to MySQL: (" . $mysqli->connect_errno . ") -> " . $mysqli->connect_error;
     die();
 }
 
